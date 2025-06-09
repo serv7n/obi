38950a5451e05cde4c78cd1278b9b7b690ff47a0
Leandro <leandro.sousa.ifpa23@gmail.com>
2025-06-09 16:54:50 -0300

    first commit

---
+++ prog_nivel_2/ogro.py
@@ -0,0 +1,6 @@
+e = int(input())
+d = int(input())
+if e > d:
+    print(e + d)
+else:
+    print(2 * (d - e))
---
+++ provas/nivel_2/2020/acelerador.py
@@ -0,0 +1,11 @@
+d = int(input())
+posicao = (d - 3) % 8
+
+if posicao == 3:
+    print(1)
+
+elif posicao == 4:
+    print(2)
+    
+elif posicao == 5:
+    print(3)
